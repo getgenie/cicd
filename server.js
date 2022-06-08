4b63f5f0d7934863ef1eec4bd369a160568bb4b6
Emran <emranio@yahoo.com>
2022-06-08 15:15:13 +0600

    added docker file

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const port = 3000
 const build = '1'
 
 app.get('/', (req, res) => {
-  res.send(`Hello World! build: ${build}`)
+    res.send(`Hello World! build: ${build}`)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
+
+function closeGracefully(signal) {
+    server.close(() => {
+        console.log(`'HTTP server closed'`)
+    })
+}
+
+process.on('SIGTERM', closeGracefully)
